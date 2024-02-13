9901fb3a3b64d7457717cc0900bee5092e702178
Piotr Słomczyński <wistako@gmail.com>
2024-02-13 18:27:48 +0100

    Change ActionName in ProductsRedux

--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -10,7 +10,7 @@ export const getCompare = ({ products }) =>
 
 /* action name creator */
 const createActionName = name => `app/products/${name}`;
-const CHANGE_COMPARE = createActionName('ADD_TO_COMPARE');
+const CHANGE_COMPARE = createActionName('CHANGE_COMPARE');
 
 /* action types */
 export const changeCompare = payload => ({ payload, type: CHANGE_COMPARE });
