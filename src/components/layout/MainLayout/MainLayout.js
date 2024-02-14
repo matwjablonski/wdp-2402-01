57cf48670c2475172ba7b5f599ece138608303e1
Piotr Słomczyński <wistako@gmail.com>
2024-02-14 11:48:33 +0100

    Add listener to MainLayout and connect with redux

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import { setRwdMode } from '../../../redux/rwdModeRedux';
 
-const MainLayout = ({ children }) => (
-  <div>
-    <Header />
-    {children}
-    <Footer />
-  </div>
-);
+const MainLayout = ({ children }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const handleResize = () => {
+      const windowWidth = window.innerWidth;
+      if (windowWidth < 576) {
+        dispatch(setRwdMode('mobile'));
+      } else if (windowWidth < 992) {
+        dispatch(setRwdMode('tablet'));
+      } else {
+        dispatch(setRwdMode('desktop'));
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
 
 MainLayout.propTypes = {
   children: PropTypes.node,
