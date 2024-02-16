e48372c4febf6e84a23922e4e569b39031f7cace
Piotr Słomczyński <wistako@gmail.com>
2024-02-16 16:32:06 +0100

    Implement component

--- src/components/features/BrandsBar/BrandsBar.js
+++ src/components/features/BrandsBar/BrandsBar.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import styles from './BrandsBar.module.scss';
+import { useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 const BrandsBar = () => {
-  return <div className={styles.root}></div>;
+  const brands = useSelector(state => state.brands);
+
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className={styles.panelBar}>
+          <div className='row align-items-center justify-content-between'>
+            <div className={styles.leftArrow}>
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </div>
+            {brands.map(brand => (
+              <div key={brand.id} className={styles.brand}>
+                <img src={brand.image} alt={brand.name} />
+              </div>
+            ))}
+            <div className={styles.rightArrow}>
+              <FontAwesomeIcon icon={faChevronRight} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BrandsBar;
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -666,6 +666,38 @@ const initialState = {
   cart: {
     products: [],
   },
+  brands: [
+    {
+      id: 'brand-1',
+      name: 'Brand 1',
+      image: process.env.PUBLIC_URL + '/images/brands/brand_1.jpg',
+    },
+    {
+      id: 'brand-2',
+      name: 'Brand 2',
+      image: process.env.PUBLIC_URL + '/images/brands/brand_2.jpg',
+    },
+    {
+      id: 'brand-3',
+      name: 'Brand 3',
+      image: process.env.PUBLIC_URL + '/images/brands/brand_3.jpg',
+    },
+    {
+      id: 'brand-4',
+      name: 'Brand 4',
+      image: process.env.PUBLIC_URL + '/images/brands/brand_4.jpg',
+    },
+    {
+      id: 'brand-5',
+      name: 'Brand 5',
+      image: process.env.PUBLIC_URL + '/images/brands/brand_5.jpg',
+    },
+    {
+      id: 'brand-6',
+      name: 'Brand 6',
+      image: process.env.PUBLIC_URL + '/images/brands/brand_6.jpg',
+    },
+  ],
 };
 
 export default initialState;
