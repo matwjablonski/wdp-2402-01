cf5cba305d09522fe0456ba808fd41260a108fad
Piotr Słomczyński <wistako@gmail.com>
2024-02-16 15:11:25 +0100

    create component

---
+++ src/components/features/BrandsBar/BrandsBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './BrandsBar.module.scss';
+
+const BrandsBar = () => {
+  return <div className={styles.root}></div>;
+};
+
+export default BrandsBar;
