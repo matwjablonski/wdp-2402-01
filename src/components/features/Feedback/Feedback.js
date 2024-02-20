256046a837f48e3c558d64f0c5ab1ba8d80f766d
Magdalena Bień <bien@magda.krakow.pl>
2024-02-20 22:49:38 +0100

    Add Swipeable

--- src/components/features/Feedback/Feedback.js
+++ src/components/features/Feedback/Feedback.js
@@ -49,44 +49,46 @@ const Feedback = () => {
   }
 
   return (
-    <div className={styles.root}>
-      <div className='container'>
-        <div className={styles.panelBar}>
-          <div className='row no-gutters align-items-end flex-column flex-md-row'>
-            <div className={'col-auto ' + styles.heading}>
-              <h3>CLIENT FEEDBACK</h3>
-            </div>
-            <div className='col'></div>
-            <div className={'col-auto ' + styles.dots}>
-              <ul>{dots}</ul>
+    <div className={`${styles.root} ${isAnimating ? styles.fade : ''}`}>
+      <Swipeable leftAction={handleSwipeLeft} rightAction={handleSwipeRight}>
+        <div className='container'>
+          <div className={styles.panelBar}>
+            <div className='row no-gutters align-items-end flex-column flex-md-row'>
+              <div className={'col-auto ' + styles.heading}>
+                <h3>CLIENT FEEDBACK</h3>
+              </div>
+              <div className='col'></div>
+              <div className={'col-auto ' + styles.dots}>
+                <ul>{dots}</ul>
+              </div>
             </div>
           </div>
+          {opinions.slice(activePage, activePage + 1).map(opinion => (
+            <div key={opinion.id} className={styles.opinionBox}>
+              <div className={styles.quote}>
+                <FontAwesomeIcon icon={faQuoteRight} />
+              </div>
+              <p className={styles.opinionContent}>{opinion.content}</p>
+              <div
+                className={'row justify-content-center flex-row' + styles.signatureBox}
+              >
+                <div className={styles.imgBox}>
+                  <img
+                    src={opinion.image}
+                    alt={opinion.name}
+                    className={styles.imgOpinion}
+                  />
+                </div>
+                <div className={styles.imgDescription}>
+                  <span className={styles.bold}>{opinion.name}</span>
+                  <br />
+                  {opinion.position}
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-        {opinions.slice(activePage, activePage + 1).map(opinion => (
-          <div key={opinion.id} className={styles.opinionBox}>
-            <div className={styles.quote}>
-              <FontAwesomeIcon icon={faQuoteRight} />
-            </div>
-            <p className={styles.opinionContent}>{opinion.content}</p>
-            <div
-              className={'row justify-content-center flex-row' + styles.signatureBox}
-            >
-              <div className={styles.imgBox}>
-                <img
-                  src={opinion.image}
-                  alt={opinion.name}
-                  className={styles.imgOpinion}
-                />
-              </div>
-              <div className={styles.imgDescription}>
-                <span className={styles.bold}>{opinion.name}</span>
-                <br />
-                {opinion.position}
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      </Swipeable>
     </div>
   );
 };
