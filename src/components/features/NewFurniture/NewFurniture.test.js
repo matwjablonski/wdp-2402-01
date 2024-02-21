e1bf0cb1fd068aacf1b6ae208fa0dea49aae0281
Piotr Słomczyński <wistako@gmail.com>
2024-02-21 14:32:39 +0100

    Remove redux-mock-store, and fix NewFurniture.test.js

--- src/components/features/NewFurniture/NewFurniture.test.js
+++ src/components/features/NewFurniture/NewFurniture.test.js
@@ -1,39 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import NewFurniture from './NewFurniture';
-import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
+import store from '../../../redux/store';
 
 describe('Component NewFurniture', () => {
-  const mockStore = configureStore();
-  let store;
-
-  beforeEach(() => {
-    store = mockStore({
-      products: {
-        data: [
-          {
-            id: '1',
-            name: 'Table',
-            price: 100,
-            image: 'table.jpg',
-            category: 'table',
-          },
-          {
-            id: '2',
-            name: 'Chair',
-            price: 50,
-            image: 'chair.jpg',
-            category: 'chair',
-          },
-        ],
-      },
-      rwdMode: {
-        rwdMode: 'desktop',
-      },
-    });
-  });
-
   it('should render without crashing', () => {
     const component = shallow(
       <Provider store={store}>
