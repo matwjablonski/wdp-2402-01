ea3971e0d88bcf37ce651e570dd8913913be2bcf
Magdalena Bień <bien@magda.krakow.pl>
2024-02-24 12:11:35 +0100

    test5

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -1,22 +1,20 @@
 import React from 'react';
 //import { useParams } from 'react-router';
-//import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 // import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
 //import ProductBox from '../../common/ProductBox/ProductBox';
-//import { getProductsByCategory } from '../../../redux/productsRedux';
+import { getProductsByCategory } from '../../../redux/productsRedux';
 
 const ProductList = () => {
   //const { categoryId } = useParams();
   const categoryId = 'chair';
 
-  /*
   const categoryProducts = useSelector(state =>
     getProductsByCategory(state, categoryId)
   );
   if (Array.isArray(categoryProducts))
     console.log('categoryProducts - to jest tablica', categoryProducts);
-  */
 
   return (
     <div className={styles.root}>
