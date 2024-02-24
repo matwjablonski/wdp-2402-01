11b815f0fdd6d51033ed5f641fbfeb6da844def8
Magdalena Bień <bien@magda.krakow.pl>
2024-02-24 10:51:19 +0100

    test2

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 // import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
+import ProductBox from '../../common/ProductBox/ProductBox';
+import { getAll } from '../../../redux/productsRedux';
 
-const ProductList = () => <div className={styles.root}>This is ProductList</div>;
+const ProductList = () => {
+  const productsAll = useSelector(state => getAll(state));
+  console.log(typeof productsAll, productsAll);
+
+  return <div className={styles.root}>sss</div>;
+};
 
 // ProductList.propTypes = {};
 
