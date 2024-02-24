b34179c5eb715b27339a2a23aca4f8854d47c743
Magdalena Bień <bien@magda.krakow.pl>
2024-02-24 17:04:56 +0100

    test2

--- src/components/views/ProductList/ProductList.test.js
+++ src/components/views/ProductList/ProductList.test.js
@@ -1,32 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ProductList from './ProductList';
-import * as reactRedux from 'react-redux';
 import { Provider } from 'react-redux';
 import store from '../../../redux/store';
 
-describe('Component ProductList', () => {
-  const useSelectorMock = jest.spyOn(reactRedux, 'useSelector');
-  //const useDispatchMock = jest.spyOn(reactRedux, 'useDispatch');
-
-  beforeEach(() => {
-    useSelectorMock.mockClear();
-    // useDispatchMock.mockClear();
-  });
-
-  it('should render without crashing', () => {
-    //const dummyDispatch = jest.fn();
-    const dummyUseSelector = jest.fn();
-    //useDispatchMock.mockReturnValue(dummyDispatch);
-    useSelectorMock.mockReturnValue(dummyUseSelector);
-    expect(useSelectorMock).not.toHaveBeenCalled();
-    //expect(dummyDispatch).not.toHaveBeenCalled();
-
+describe('ProductList', () => {
+  it('renders without crashing', () => {
     const component = shallow(
       <Provider store={store}>
         <ProductList />
       </Provider>
     );
-    expect(component).toBeTruthy();
   });
 });
